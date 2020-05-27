6e50f20dfb3a7021505aa080dd0d026acd352396
Lachlan Sneff <lachlan.sneff@gmail.com>
2020-05-27 18:20:21 -0400

    Remove dupped license header

--- src/scene.rs
+++ src/scene.rs
@@ -3,9 +3,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use anyhow::{Context, Result};
 use std::{mem, slice, sync::Arc, thread};
