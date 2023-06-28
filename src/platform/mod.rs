180faed0b4b784d392209c6039079a229fdbdd7c
Mark Friedenbach <mark@friedenbach.org>
2023-06-28 15:44:55 -0700

    Fix support for target_os = "linux"

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -8,6 +8,11 @@
 // public.
 mod defaults;
 
+#[cfg(target_os = "linux")]
+pub mod linux;
+#[cfg(target_os = "linux")]
+pub use self::linux::*;
+
 #[cfg(target_os = "macos")]
 pub mod macos;
 #[cfg(target_os = "macos")]
