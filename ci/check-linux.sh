06e4efe5a7bb6973cfbc99bd47e02fdd5abae50f
Mark Friedenbach <mark@friedenbach.org>
2023-07-03 14:26:38 -0700

    Do cross compilation checks on linux host, as Github costs are less.

--- ci/check-linux.sh
+++ ci/check-linux.sh
@@ -3,6 +3,22 @@
 set -e
 
 HOSTS=$(cat << EOM
+aarch64-linux-android
+thumbv7neon-linux-androideabi
+x86_64-linux-android
+i686-linux-android
+x86_64-unknown-linux-gnu
+i686-unknown-linux-gnu
+riscv64gc-unknown-linux-gnu
+aarch64-unknown-linux-gnu
+thumbv7neon-unknown-linux-gnueabihf
+powerpc64-unknown-linux-gnu
+powerpc64le-unknown-linux-gnu
+x86_64-pc-windows-msvc
+x86_64-pc-windows-gnu
+i686-pc-windows-msvc
+i686-pc-windows-gnu
+aarch64-pc-windows-msvc
 EOM
 )
 
