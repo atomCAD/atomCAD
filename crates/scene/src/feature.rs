8eba7a8f54e7b7aa40056a8c6777aa7fb0466e75
Seth Hinz <sethhinz@me.com>
2023-08-14 13:22:05 -0700

    part one of feature overhaul using full atom lineage

--- crates/scene/src/feature.rs
+++ crates/scene/src/feature.rs
@@ -1,206 +1,26 @@
-use std::collections::HashMap;
+use std::{borrow::Borrow, collections::HashMap};
 
 use periodic_table::Element;
 
-use crate::{
-    molecule::{self, AtomIndex},
-    BondOrder, Molecule,
-};
-use std::borrow::Borrow;
+use crate::{ids::*, molecule::AtomIndex, BondOrder, Molecule};
 
-pub type FeatureIndex = usize;
-
-/// Specifies where an atom's data originially came from. For example, although
-/// a mirror feature is directly responsible for creating many atoms,
-/// it does not own any of them - their original data comes from some other feature
-/// or set of features which are being mirrored. Note that if an atom is `DerivedFrom` another
-/// feature's data, that feature might not be the owner - for example, if you mirror an atom
-/// twice, you will need to trace it back until the AtomSource is `Owned` - the feature which
-/// owns an atom will be the original source of its data.
-pub enum AtomSource {
-    Owned,
-    // `DerivedFrom` tells you the feature index in the FeatureList where an atom's data came from
-    // most recently. It also tells you what atom index that it is stored under in that feature.
-    // I.e. in simple setup like:
-    //
-    // FeatureList:
-    // feature 0: Create atom 0
-    // feature 1: mirror feature 0 (creates atom 1)
-    // feature 2: mirror feature 1 (creates atom 2)
-    //
-    // Then feature2.get_atom_source() would return AtomSource::DerivedFrom(1, 1), because atom 2
-    // is a mirror of AtomIndex 1, which is owned by feature 1. feature2.get_atom_source_recursive()
-    // would return AtomSource::DerivedFrom(0, 0), because the atom source is owned by
-    // feature 0 and has AtomIndex 0 in that context.
-    DerivedFrom(FeatureIndex, AtomIndex),
+pub trait MoleculeCommands {
+    // fn find_atom(&self, spec: &AtomSpecifier) -> Option<AtomIndex>;
+    fn add_atom(&mut self, element: Element, pos: ultraviolet::Vec3, spec: AtomSpecifier);
+    fn create_bond(&mut self, a1: &AtomSpecifier, a2: &AtomSpecifier, order: BondOrder);
 }
 
 pub trait Feature {
-    fn apply(&self, features: &FeatureList, molecule: &mut Molecule);
-    fn depends_on(&self, other_feature: FeatureIndex) -> bool;
-    /// Returns a slice containing all of the AtomIndexes that this feature was directly
-    /// responsible for creating. i.e. every atom that was created due to running `self.apply`.
-    fn get_atoms(&self) -> &[molecule::AtomIndex];
-    // fn get_dependency(&self, dependency_id: usize) -> Option<FeatureIndex>;
-    // fn each_dependency(&self, f: impl FnMut(FeatureIndex) -> ());
-
-    /// Returns an AtomSource stating where the atom with index `atom_index`
-    /// got its data from.
-    fn get_atom_source(&self, atom_index: AtomIndex) -> AtomSource;
-
-    fn get_atom_source_recursive(
-        &self,
-        features: &FeatureList,
-        mut atom_index: AtomIndex,
-    ) -> AtomSource {
-        let mut feature_id;
-
-        match self.get_atom_source(atom_index) {
-            AtomSource::DerivedFrom(new_feature_id, new_atom_index) => {
-                feature_id = new_feature_id;
-                atom_index = new_atom_index
-            }
-            AtomSource::Owned => return AtomSource::Owned,
-        }
-
-        loop {
-            // TODO: Error handle in a way the user can see
-            let feature = features
-                .get(feature_id)
-                .expect("A valid feature's dependent atom should belong to another valid feature!");
-            match feature.get_atom_source(atom_index) {
-                AtomSource::DerivedFrom(new_feature_id, new_atom_index) => {
-                    feature_id = new_feature_id;
-                    atom_index = new_atom_index;
-                }
-                AtomSource::Owned => return AtomSource::DerivedFrom(feature_id, atom_index),
-            }
-        }
-    }
-}
-
-// Creates a molecule and does not depend on any other features.
-pub struct MoleculeFeature {
-    root_atom: [AtomIndex; 1],
-}
-
-impl MoleculeFeature {
-    pub fn new(root_atom: AtomIndex) -> Self {
-        MoleculeFeature {
-            root_atom: [root_atom],
-        }
-    }
-}
-
-impl Feature for MoleculeFeature {
-    fn apply(&self, _features: &FeatureList, _molecule: &mut Molecule) {
-        panic!("MoleculeFeature is not meant to be applied - it is just used to give the root atom an owning feature.")
-    }
-
-    fn depends_on(&self, other_feature: FeatureIndex) -> bool {
-        false
-    }
-
-    fn get_atoms(&self) -> &[molecule::AtomIndex] {
-        &self.root_atom
-    }
-
-    fn get_atom_source(&self, atom_index: AtomIndex) -> AtomSource {
-        if atom_index == self.root_atom[0] {
-            AtomSource::Owned
-        } else {
-            // TODO: error handle
-            panic!("get_atom_source called for an atom that doesn't belong to this feature!");
-        }
-    }
-}
-
-#[derive(Debug)]
-struct FeatureError;
-
-pub struct AtomSpecifier {
-    temp: AtomIndex,
-    // An atom is specified first by referencing the feature that owns the atom (i.e.,
-    // the newest feature in the edit history that, when removed, removes this atom.)
-    // For example, if an atom is created by mirroring another feature,
-    // then the feature_id in its AtomSpecifier is the mirror feature, not the
-    // feature which created the mirror.
-    feature_id: FeatureIndex,
-    // The feature path specifies how to reach the base feature that directly holds
-    // the atom data. Walking the dependencies of the feature with id `feature_id` will
-    // bring you to the terminal feature which directly owns a referenced atom.
-    feature_path: Vec<usize>,
-    // Once the terminal feature is found, this is the atom id within that feature. It is
-    // used to look up the actual instantiation of an atom.
-    atom_id: usize,
-}
-
-impl AtomSpecifier {
-    fn depends_on(&self, other_feature: FeatureIndex) -> bool {
-        false
-    }
-
-    fn resolve(&self, features: &FeatureList) -> Result<AtomIndex, FeatureError> {
-        Ok(self.temp)
-    }
-}
-
-pub struct AtomFeature {
-    element: Element,
-    target: AtomSpecifier,
-    bond_order: BondOrder,
-}
-
-impl AtomFeature {
-    pub fn new(element: Element, target: AtomSpecifier, bond_order: BondOrder) -> Self {
-        AtomFeature {
-            element,
-            target,
-            bond_order,
-        }
-    }
-}
-
-impl Feature for AtomFeature {
-    fn apply(&self, features: &FeatureList, molecule: &mut Molecule) {
-        molecule.add_atom(
-            self.element,
-            // TODO: error handle
-            self.target.resolve(features).unwrap(),
-            self.bond_order,
-            None,
-        );
-    }
-
-    fn depends_on(&self, other_feature: FeatureIndex) -> bool {
-        self.target.depends_on(other_feature)
-    }
-
-    fn get_atoms(&self) -> &[molecule::AtomIndex] {
-        &[]
-    }
-}
-
-/// Refers to a specific derived feature. For example, to refer to the second copy of a feature created by a linear pattern
-/// with ID 3, create FeatureCopyReference { feature_id: 3, copy_id: 1 }
-pub struct FeatureCopyReference {
-    feature_id: FeatureIndex,
-    copy_id: usize
-}
-
-pub type AtomSpecifier = Vec<FeatureCopyReference>
-
-pub trait MoleculeCommands {
-    pub fn find_atom(specifier: AtomSpecifier) -> AtomIndex;
+    fn apply(&self, commands: &mut dyn MoleculeCommands);
 }
 
 // A container that stores a list of features. It allows the list to be manipulated without
-// changing the indexes of existing features,
+// changing the indexes of existing features
 #[derive(Default)]
 pub struct FeatureList {
     counter: usize,
-    order: Vec<FeatureIndex>,
-    features: HashMap<FeatureIndex, Box<dyn Feature>>,
+    order: Vec<FeatureId>,
+    features: HashMap<FeatureId, Box<dyn Feature>>,
 }
 
 impl FeatureList {
@@ -216,12 +36,12 @@ impl FeatureList {
     }
 
     // Removes the feature with the given `id` from the feature list, shifting all features after it to the left.
-    pub fn remove(&mut self, id: FeatureIndex) {
+    pub fn remove(&mut self, id: FeatureId) {
         self.features.remove(&id);
         self.order.remove(id);
     }
 
-    pub fn get(&self, id: FeatureIndex) -> Option<&dyn Feature> {
+    pub fn get(&self, id: FeatureId) -> Option<&dyn Feature> {
         self.features.get(&id).map(|bo| bo.borrow())
     }
 
--- crates/scene/src/molecule.rs
+++ crates/scene/src/molecule.rs
@@ -1,4 +1,4 @@
-use std::iter::Empty;
+use std::{collections::HashMap, iter::Empty};
 
 use periodic_table::Element;
 use petgraph::{
@@ -9,6 +9,11 @@ use petgraph::{
 use render::{AtomKind, AtomRepr, Atoms, GlobalRenderResources};
 use ultraviolet::Vec3;
 
+use crate::{
+    feature::{FeatureList, MoleculeCommands},
+    ids::{AtomSpecifier, FeatureCopyId},
+};
+
 type Graph = stable_graph::StableUnGraph<AtomNode, BondOrder>;
 pub type BondOrder = u8;
 pub type AtomIndex = stable_graph::NodeIndex;
@@ -17,6 +22,7 @@ pub type BondIndex = stable_graph::EdgeIndex;
 struct AtomNode {
     element: Element,
     pos: Vec3,
+    spec: AtomSpecifier,
 }
 
 pub struct Molecule {
@@ -25,21 +31,32 @@ pub struct Molecule {
     rotation: ultraviolet::Rotor3,
     offset: ultraviolet::Vec3,
     gpu_synced: bool,
+    feature_list: FeatureList,
+    // TODO: This atom map is a simple but extremely inefficient implementation. This data
+    // is highly structued and repetitive: compression, flattening, and a tree could do
+    // a lot to optimize this.
+    atom_map: HashMap<AtomSpecifier, AtomIndex>,
 }
 
 impl Molecule {
     // Creates a `Molecule` containing just one atom. At the moment, it is not possible
     // to construct a `Molecule` with no contents, as wgpu will panic if an empty gpu buffer
     // is created
-    pub fn from_first_atom(
-        gpu_resources: &GlobalRenderResources,
-        first_atom: Element,
-    ) -> (Self, NodeIndex) {
+    pub fn from_first_atom(gpu_resources: &GlobalRenderResources, first_atom: Element) -> Self {
         let mut graph = Graph::default();
+        let spec = AtomSpecifier {
+            feature_path: vec![FeatureCopyId {
+                feature_id: 0,
+                copy_index: 0,
+            }],
+            child_index: 0,
+        };
 
         let first_index = graph.add_node(AtomNode {
             element: first_atom,
             pos: Vec3::default(),
+            // TODO: This needs to either come from a feature or be returned
+            spec: spec.clone(),
         });
 
         let gpu_atoms = Atoms::new(
@@ -50,41 +67,15 @@ impl Molecule {
             }],
         );
 
-        (
-            Molecule {
-                gpu_atoms,
-                graph,
-                rotation: ultraviolet::Rotor3::default(),
-                offset: ultraviolet::Vec3::default(),
-                gpu_synced: false,
-            },
-            first_index,
-        )
-    }
-
-    pub fn add_atom(
-        &mut self,
-        element: Element,
-        bond_to: NodeIndex,
-        bond_order: BondOrder,
-        gpu_resources: Option<&GlobalRenderResources>,
-    ) -> NodeIndex {
-        // Create the node on the graph
-        let new_node = self.graph.add_node(AtomNode {
-            element,
-            // TODO: compute pos from the atom being bonded to
-            pos: Vec3::new(5., 0., 0.),
-        });
-        self.graph.add_edge(new_node, bond_to, bond_order);
-
-        if let Some(gpu_resources) = gpu_resources {
-            // Synchronize with the GPU
-            self.reupload_atoms(gpu_resources);
-        } else {
-            self.gpu_synced = false;
+        Molecule {
+            gpu_atoms,
+            graph,
+            rotation: ultraviolet::Rotor3::default(),
+            offset: ultraviolet::Vec3::default(),
+            gpu_synced: false,
+            feature_list: Default::default(),
+            atom_map: HashMap::from([(spec, first_index)]),
         }
-
-        new_node
     }
 
     pub fn reupload_atoms(&mut self, gpu_resources: &GlobalRenderResources) {
@@ -109,4 +100,18 @@ impl Molecule {
     pub fn atoms(&self) -> &Atoms {
         &self.gpu_atoms
     }
+}
+
+impl MoleculeCommands for Molecule {
+    fn add_atom(&mut self, element: Element, pos: ultraviolet::Vec3, spec: AtomSpecifier) {
+        let index = self.graph.add_node(AtomNode {
+            element,
+            pos,
+            spec: spec.clone(),
+        });
+        self.atom_map.insert(spec, index);
+        self.gpu_synced = false;
+    }
+
+    fn create_bond(&mut self, a1: &AtomSpecifier, a2: &AtomSpecifier, order: BondOrder) {}
 }
