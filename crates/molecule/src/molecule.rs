885f9f549bf094a341db817857602e1772acbcd4
Seth Hinz <sethhinz@me.com>
2023-09-17 21:43:41 -0700

    Fixed bounding box bug when restoring from checkpoints. Added atom removal

--- crates/molecule/src/molecule.rs
+++ crates/molecule/src/molecule.rs
@@ -209,6 +209,10 @@ impl Molecule {
         for (atom_index, atom) in self.graph.node_references() {
             self.atom_map.insert(atom.spec.clone(), atom_index);
         }
+
+        // We have to recomputue the bounding box, as it is not
+        // stored in a checkpoint
+        self.recompute_bounding_box();
     }
 
     pub fn make_checkpoint(&self) -> MoleculeCheckpoint {
@@ -220,6 +224,15 @@ impl Molecule {
 
     pub fn bounding_box(&self) -> &BoundingBox {
         &self.bounding_box
+    }
+
+    pub fn recompute_bounding_box(&mut self) {
+        for atom in self.graph.node_weights() {
+            self.bounding_box.enclose_sphere(
+                *self.positions.get(&atom.spec).unwrap(),
+                PERIODIC_TABLE.element_reprs[atom.element as usize].radius,
+            );
+        }
     }
 
     // TODO: Optimize heavily (use octree, compute entry point of ray analytically)
@@ -293,7 +306,6 @@ impl Molecule {
         None
     }
 }
-
 impl EditContext for Molecule {
     fn add_bonded_atom(
         &mut self,
@@ -332,6 +344,26 @@ impl EditContext for Molecule {
         );
         self.gpu_synced = false;
         self.positions.insert(spec, pos);
+
+        Ok(())
+    }
+
+    fn remove_atom(&mut self, target: &AtomSpecifier) -> Result<(), EditError> {
+        let index = self
+            .atom_map
+            .get(target)
+            .ok_or(EditError::BrokenReference(ReferenceType::Atom))?;
+
+        self.graph.remove_node(*index);
+
+        self.atom_map
+            .remove(target)
+            .ok_or(EditError::BrokenReference(ReferenceType::Atom))?;
+        self.positions
+            .remove(target)
+            .ok_or(EditError::BrokenReference(ReferenceType::Atom))?;
+        self.recompute_bounding_box();
+        self.gpu_synced = false;
 
         Ok(())
     }
