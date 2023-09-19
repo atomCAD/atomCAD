9d27a4cb9d38dd3e3db46eb4ecf6958d24cd7f2b
Mark Friedenbach <mark@friedenbach.org>
2023-09-18 21:53:07 -0700

    Add code docs and reorder Cargo.toml entries.

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,12 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "atomCAD".to_string(),
+                // FIXME: this should be read from a persistent settings file
                 resolution: (800., 600.).into(),
-                // Bind to canvas included in `index.html`
+                // Bind to canvas included in `index.html` on web
                 canvas: Some("#bevy".to_owned()),
-                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                // Tells wasm not to override default event handling,
+                // like F5 and Ctrl+R
                 prevent_default_event_handling: false,
                 ..default()
             }),
