f0836c2191e30500df9c89d4a5c6d07bddf59555
Mark Friedenbach <mark@friedenbach.org>
2023-09-20 12:11:40 -0700

    Add clarifying code doc.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// Bevy uses some very complex types for specifying system inputs.
+// There's just no getting around this, so silence clippy's complaints.
 #![allow(clippy::type_complexity)]
 
 mod actions;
