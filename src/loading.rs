8c3b6a2de9825e6c3cf489c843bed81ab849d0bd
Mark Friedenbach <mark@friedenbach.org>
2024-01-21 13:05:50 -0800

    Update to bevy_asset_loader 0.19

--- src/loading.rs
+++ src/loading.rs
@@ -14,10 +14,11 @@ pub struct LoadingPlugin;
 impl Plugin for LoadingPlugin {
     fn build(&self, app: &mut App) {
         app.add_loading_state(
-            LoadingState::new(AppState::Loading).continue_to_state(AppState::SplashScreen),
-        )
-        .add_collection_to_loading_state::<_, FontAssets>(AppState::Loading)
-        .add_collection_to_loading_state::<_, TextureAssets>(AppState::Loading);
+            LoadingState::new(AppState::Loading)
+                .continue_to_state(AppState::SplashScreen)
+                .load_collection::<FontAssets>()
+                .load_collection::<TextureAssets>(),
+        );
     }
 }
 
