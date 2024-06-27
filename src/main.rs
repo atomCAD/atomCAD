25a1a3aafad9388822efdeb786225939122943c2
Mark Friedenbach <mark@friedenbach.org>
2024-06-27 14:53:24 -0700

    Initial import.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins).run();
+}
+
+// End of File
