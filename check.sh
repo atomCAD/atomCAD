dad2b08789b53fdd7479118afac07a78aea3178b
Mark Friedenbach <mark@friedenbach.org>
2024-06-29 02:09:53 -0700

    Add check.sh, a shell script for performing most CI checks locally.

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+echo Checking syntax...
+cargo check # native
+cargo check --target wasm32-unknown-unknown # web
+
+echo Running tests...
+cargo test --workspace --all-features
+
+echo Running linter check...
+cargo clippy --workspace --all-targets --all-features -- -D warnings # native
+cargo clippy --workspace --target wasm32-unknown-unknown --all-targets --all-features -- -D warnings # web
+
+echo Running formatting check...
+cargo fmt --all -- --check
+
+echo All done!
+
+# End of file
