1df32966379d5ef32e04ac08d26cc9ba50ae9720
Mark Friedenbach <mark@friedenbach.org>
2024-06-30 10:37:57 -0700

    Don't launch logging console on Windows in release builds.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// Disable console on windows in release builds.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use bevy::prelude::*;
 
 use atomcad::{platform::bevy::PlatformTweaks, AppPlugin, APP_NAME};
