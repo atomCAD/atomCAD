7275cb13b002414debd1fe6a02e452b791c625e5
Ádám Nagy <nadam60@gmail.com>
2025-03-11 19:21:51 +0100

    increasing evaluation volume

--- rust/src/kernel/common_constants.rs
+++ rust/src/kernel/common_constants.rs
@@ -1,7 +1,7 @@
 // TODO: these will not be constant, will be set by the user
 use glam::i32::IVec3;
 
-pub const IMPLICIT_VOLUME_MIN: IVec3 = IVec3::new(-4, -4, -4);
-pub const IMPLICIT_VOLUME_MAX: IVec3 = IVec3::new(4, 4, 4);
+pub const IMPLICIT_VOLUME_MIN: IVec3 = IVec3::new(-10, -10, -10);
+pub const IMPLICIT_VOLUME_MAX: IVec3 = IVec3::new(10, 10, 10);
 
 pub const DIAMOND_UNIT_CELL_SIZE_ANGSTROM: f32 = 3.567;  // Size of one complete unit cell in Ångströms
