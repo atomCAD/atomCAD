cd1289bcd023fc2220382baa5338a4743ba38f75
Ádám Nagy <nadam60@gmail.com>
2025-03-18 09:31:14 +0100

    editor selection on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: const SceneComposer(),
+    return const MaterialApp(
+      home: EditorSelector(),
+    );
+  }
+}
+
+class EditorSelector extends StatelessWidget {
+  const EditorSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('atomCAD Editor Selection'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const Scaffold(
+                      body: StructureDesigner(),
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Structure Designer'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const Scaffold(
+                      body: SceneComposer(),
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Scene Composer'),
+            ),
+          ],
+        ),
       ),
     );
   }
