bae142dd6619182a301bbf98b84fbd9b72e09cbd
Ádám Nagy <nadam60@gmail.com>
2025-03-18 20:48:20 +0100

    menu bar in scene composer

--- lib/scene_composer/scene_composer.dart
+++ lib/scene_composer/scene_composer.dart
@@ -15,10 +15,64 @@ class _SceneComposerState extends State<SceneComposer> {
     super.initState();
   }
 
+  void _importXYZ() {
+    // TODO: Implement XYZ file import functionality
+    debugPrint('Import XYZ file selected');
+  }
+
+  void _exportXYZ() {
+    // TODO: Implement XYZ file export functionality
+    debugPrint('Export XYZ file selected');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Container(
+          height: 30,
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.black26,
+                width: 1,
+              ),
+            ),
+          ),
+          child: Row(
+            children: [
+              MenuAnchor(
+                builder: (context, controller, child) {
+                  return TextButton(
+                    onPressed: () {
+                      if (controller.isOpen) {
+                        controller.close();
+                      } else {
+                        controller.open();
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.black87,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                    ),
+                    child: const Text('File'),
+                  );
+                },
+                menuChildren: [
+                  MenuItemButton(
+                    onPressed: _importXYZ,
+                    child: const Text('Import XYZ'),
+                  ),
+                  MenuItemButton(
+                    onPressed: _exportXYZ,
+                    child: const Text('Export XYZ'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
         Center(
           child: SizedBox(
             width: 1280,
