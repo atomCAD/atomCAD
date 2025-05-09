58b42d244534dfc3850b40a40a065ee4201b00f6
Mark Friedenbach <mark@friedenbach.org>
2025-05-09 11:31:46 -0700

    winit: add atomcad-winit-runner crate with event loop integration
    Introduce atomcad-winit-runner, which provides WinitPlugin for managing
    the winit event loop lifecycle. The plugin inserts a
    WinitEventLoopBuilder as a non-Send resource during registration, then
    builds the EventLoop during finalize (after other plugins have had a
    chance to configure the builder).
    
    The root crate is reworked to use a real winit event loop runner
    implementing ApplicationHandler. The runner extracts the WinitEventLoop
    from the world, calls run_app(), and drives App::update() from the
    about_to_wait callback. The hello_world function becomes a Startup
    system that sends an AppExit message to request clean shutdown after the
    first tick.

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,74 @@
 // You can obtain one at <https://mozilla.org/MPL/2.0/>.
 
 use app::prelude::*;
+use ecs::prelude::*;
 use logging::prelude::*;
 use window::prelude::*;
+use winit::{
+    application::ApplicationHandler, event::WindowEvent, event_loop::ActiveEventLoop,
+    window::WindowId,
+};
+use winit_runner::{WinitEventLoop, WinitPlugin};
 
 pub const APP_NAME: &str = "atomCAD";
 
-fn hello_world(app: &mut App) -> AppExit {
-    let _ = app;
+struct Application<'app> {
+    app: &'app mut App,
+}
+
+impl<'app> Application<'app> {
+    fn new(app: &'app mut App) -> Self {
+        Self { app }
+    }
+}
+
+impl ApplicationHandler<()> for Application<'_> {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        _event: WindowEvent,
+    ) {
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        self.app.update();
+        if self.app.should_exit().is_some() {
+            // Someone, somewhere has told us to quit.  Exit the event loop.
+            //
+            // FIXME: Should we close all windows first?
+            //
+            // FIXME: There should be some way to pass the AppExit value returned by
+            //        [`App::should_exit()`] to the event loop, so that the whole process will
+            //        terminate with the specified exit code.  It is not clear how to do this with
+            //        winit's current API.
+            event_loop.exit();
+        }
+    }
+}
+
+fn runner(app: &mut App) -> AppExit {
+    let WinitEventLoop(event_loop) = app
+        .remove_non_send::<WinitEventLoop<()>>()
+        .expect("Cannot find EventLoop in world");
+    let mut app = Application::new(app);
+    match event_loop.run_app(&mut app) {
+        Ok(_) => {
+            log::info!("Event loop exited cleanly.");
+            AppExit::Success
+        }
+        Err(err) => {
+            log::error!("Event loop exited with error: {}", err);
+            AppExit::Error(std::num::NonZeroU8::new(1).unwrap())
+        }
+    }
+}
+
+fn hello_world(mut messages: MessageWriter<AppExit>) {
     log::info!("Hello, World!");
-    app.update();
-    app.should_exit().unwrap_or(AppExit::Success)
+    messages.write(AppExit::Success);
 }
 
 pub fn start() -> AppExit {
@@ -24,9 +82,12 @@ pub fn start() -> AppExit {
             "atomcad_keyboard",
             "atomcad_logging",
             "atomcad_window",
+            "atomcad_winit_runner",
         ]))
         .add_plugin(WindowPlugin::new(ExitCondition::DoNotExit))
-        .set_runner(hello_world)
+        .add_plugin(WinitPlugin::<()>::default())
+        .add_systems(Startup, hello_world)
+        .set_runner(runner)
         .run()
 }
 
