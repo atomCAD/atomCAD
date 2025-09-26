be4dc84706a752ecf137f881748db754790ff1d3
Ádám Nagy <nadam60@gmail.com>
2025-09-26 15:18:45 +0200

    calculate custom node type for the map node

--- rust/src/structure_designer/nodes/map.rs
+++ rust/src/structure_designer/nodes/map.rs
@@ -8,6 +8,7 @@ use crate::structure_designer::node_type_registry::NodeTypeRegistry;
 use crate::structure_designer::structure_designer::StructureDesigner;
 use crate::structure_designer::evaluator::network_evaluator::NetworkEvaluator;
 use crate::structure_designer::data_type::DataType;
+use crate::structure_designer::data_type::FunctionType;
 use crate::structure_designer::node_type::NodeType;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -21,8 +22,17 @@ impl NodeData for MapData {
       None
     }
 
-    fn calculate_custom_node_type(&self, _base_node_type: &NodeType) -> Option<NodeType> {
-      None
+    fn calculate_custom_node_type(&self, base_node_type: &NodeType) -> Option<NodeType> {
+      let mut custom_node_type = base_node_type.clone();
+
+      custom_node_type.parameters[0].data_type = DataType::Array(Box::new(self.input_type.clone()));
+      custom_node_type.parameters[1].data_type = DataType::Function(FunctionType {
+        parameter_types: vec![self.input_type.clone()],
+        output_type: Box::new(self.output_type.clone()),
+      });
+      custom_node_type.output_type = DataType::Array(Box::new(self.output_type.clone()));
+
+      Some(custom_node_type)
     }
 
     fn eval<'a>(
